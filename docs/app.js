fcba20e2134a00e63018e7d97ab42f2e4b3cc1a6
Павел Хрусталев <Pavel@MacBook-Air-Pavel.local>
2019-02-06 22:57:05 +0300

    music-commit

--- docs/app.js
+++ docs/app.js
@@ -1,10 +1,11 @@
+const audio = new Audio();
+
 let scores, roundScore, activePlayer, gameInit, finishScore;
 
 let score0 = document.getElementById('score-0');
 let score1 = document.getElementById('score-1');
 let current0 = document.getElementById('current-0');
 let current1 = document.getElementById('current-1');
-let dice = document.querySelector('.dice');
 let playerPanel0 = document.querySelector(`.player-0-panel`);
 let playerPanel1 = document.querySelector(`.player-1-panel`);
 
@@ -27,6 +28,13 @@ finishScoreValue.addEventListener('input', () => {
   finishScoreValue.classList.remove('border-error');  
 })
 
+const bgSoundInit = () => {
+  audio.src = 'music/for-background.mp3';
+  audio.volume = 0.05;
+  audio.autoplay = true;
+  audio.muted = false;
+}
+
 const init = () => {
   scores = [0, 0];
   roundScore = 0;
@@ -35,13 +43,15 @@ const init = () => {
   finishScore = 100;
   buttonScore.removeAttribute('disabled');
 
+  bgSoundInit();
 
   score0.textContent = 0;
   score1.textContent = 0;
   current0.textContent = 0;
   current1.textContent = 0;
 
-  dice.style.display = 'none';
+  document.querySelector('#dice-1').style.display = 'none';
+  document.querySelector('#dice-2').style.display = 'none';
   playerPanel0.classList.remove('active');
   playerPanel1.classList.remove('active');
   playerPanel0.classList.add('active');
@@ -54,6 +64,36 @@ const init = () => {
 
 init();
 
+const audioWinner = () => {
+  audio.src = 'music/winner.mp3';
+  audio.volume = 0.2;
+  audio.autoplay = true;
+}
+
+const audioHold = () => {
+  const audio = new Audio();
+  audio.src = 'music/hold.wav';
+  audio.volume = 0.3;
+  audio.play();
+}
+
+const noSoundBackground = () => {
+  audio.muted = true;
+  document.querySelector('.no-sound').style.display = 'none';
+  document.querySelector('.yes-sound').style.display = 'block';
+}
+
+const yesSoundBackground = () => {
+  audio.muted = false;
+  document.querySelector('.no-sound').style.display = 'block';
+  document.querySelector('.yes-sound').style.display = 'none';
+}
+
+document.querySelector('.yes-sound').addEventListener('click', yesSoundBackground);
+
+document.querySelector('.no-sound').addEventListener('click', noSoundBackground);
+
+
 const activePlayerFunc = () => {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
@@ -62,7 +102,9 @@ const activePlayerFunc = () => {
     
     playerPanel0.classList.toggle('active');
     playerPanel1.classList.toggle('active');
-    dice.style.display = 'none';
+
+    document.querySelector('#dice-1').style.display = 'none';
+    document.querySelector('#dice-2').style.display = 'none';
 }
 
 
@@ -71,17 +113,26 @@ document.querySelector('.btn-roll').addEventListener('click', () => {
   buttonScore.setAttribute('disabled', 'disabled');
 
   if (gameInit) {
-    let diceDOM = document.querySelector('.dice');
-    let dice = Math.floor(Math.random() * 6) + 1;
+    let diceDOM1 = document.querySelector('#dice-1');
+    let diceDOM2 = document.querySelector('#dice-2');
 
-    diceDOM.style.display = 'block';
+    let randomDice1 = Math.floor(Math.random() * 6) + 1;
+    let randomDice2 = Math.floor(Math.random() * 6) + 1;
+
+    diceDOM1.style.display = 'block';
+    diceDOM2.style.display = 'block';
+
+    const audio = new Audio();
+    audio.src = 'music/broke-dice.mp3';
+    audio.volume = 0.3;
+    audio.play();
     
-    diceDOM.src = `dice-${dice}.png`;
+    diceDOM1.src = `img/dice-${randomDice1}.png`;
+    diceDOM2.src = `img/dice-${randomDice2}.png`;
   
-    if (dice !== 1) {
-      roundScore += dice;
+    if (randomDice1 !== 1 && randomDice2 !== 1) {
+      roundScore += randomDice1 + randomDice2;
       document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
-  
     } else {
       activePlayerFunc();
     }
@@ -97,14 +148,15 @@ document.querySelector('.btn-hold').addEventListener('click', () => {
   if (gameInit) {
     scores[activePlayer] += roundScore;
     document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];
-  
+    audioHold();
     if (scores[activePlayer] >= finishScore) {
       document.getElementById(`name-${activePlayer}`).textContent = 'ПОБЕДА!';
+      audioWinner();
       document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
       document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
-      dice.style.display = 'none';
+      document.querySelector('#dice-1').style.display = 'none';
+      document.querySelector('#dice-2').style.display = 'none';
       gameInit = false;
-
     } else {
       activePlayerFunc();
     }
